7fdbbd11b220c0c50218a1bcd74d4c484933b038
Danila Kolchanov <79622915+DanilaOz@users.noreply.github.com>
2021-04-23 18:00:02 +0500

    Update task4.js

--- ЛАБРАБ03/task4.js
+++ ЛАБРАБ03/task4.js
@@ -2,7 +2,7 @@ const _ = require('lodash');
 const conv = require('../code/convert_csv_to_json');
 
 let file_csv1 = '../csv/groups.csv';
-let file_csv2 = '../csv/students.csv'
+let file_csv2 = '../csv/students.csv';
 
 let arr_groups = conv.csv_to_json(file_csv1);
 let arr_students = conv.csv_to_json(file_csv2);
